479158dece402d8938139db250c66319758f4b9f
Mesutio <mesutibisoglu@gmail.com>
2022-03-23 12:17:54 +0300

    created db and Post model

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express=require("express");
+const mongoose=require("mongoose");
+const Post=require("./models/Post");
 const ejs=require("ejs");
 const app=express();
 
+mongoose.connect('mongodb://localhost/cleanblog-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 app.set('view engine','ejs');
 
 app.use(express.static('public'))
-app.get("/",(req,res)=>{
-    res.render('index');
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.get('/',async (req,res)=>{
+    const posts=await Post.find({});
+    res.render('index',{
+        posts
+    });
+    //eklememişsin ki?
+//res.sendFile(path.resolve(__dirname,'temp/index.html'))
 })
 app.get("/about",(req,res)=>{
     res.render('about');
@@ -17,6 +30,10 @@ app.get("/add_post",(req,res)=>{
 app.get("/post",(req,res)=>{
     res.render('post');
 })
+app.post('/posts',async (req,res)=>{
+    await  Post.create(req.body)
+      res.redirect("/");
+  })
 const PORT=5000;
 app.listen(PORT,()=>{
     console.log(`sunucu ${PORT} da başladı`);
